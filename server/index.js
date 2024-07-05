bc7dd41fa86b8baf24e565c34393d7fb829a7cd3
Luchoromero29 <luchonromero@gmail.com>
2024-07-05 16:39:25 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+// index.js
+import express from "express";
+import { PORT } from './config.js'
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Aqui comienza Gymbros');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
